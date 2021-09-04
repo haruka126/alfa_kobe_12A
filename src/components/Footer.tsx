4abf28f96a4e7ffb07fc1baafb00044afe70ec4a
GothicLoli <gothicyoha@gmail.com>
2021-09-04 22:40:24 +0900

    フッターの必須条件を満たした

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,17 @@
-import { Box, Center } from "@chakra-ui/react";
-import { Copyright } from "./Copyright";
+import { Center, Text } from "@chakra-ui/react";
 
 type Props = {
     height: string;
     backgroundColor: string;
-    enableCopyright?: boolean;
 };
 
-export const Footer: React.VFC<Props> = ({ height, backgroundColor, enableCopyright = false }) => {
+export const Footer: React.VFC<Props> = ({ height, backgroundColor }) => {
     return (
-        <Box h={height} backgroundColor={backgroundColor}>
-            { enableCopyright && 
-            <Center>
-                <Copyright bottomJustify fontSize="14px" color="gray">Copyright &copy; 2021 12年A組</Copyright>
+        <Center h={height} backgroundColor={backgroundColor}>
+            <Center flexDirection="column" pos="absolute" bottom={0}>
+                <Text size="14px" color="gray">「リアル脱出ゲーム」は株式会社のSCRAPの登録商標です</Text>
+                <Text size="14px" color="gray">Copyright &copy; 2021 12年A組</Text>
             </Center>
-            }
-        </Box>
+        </Center>
     );
 };
