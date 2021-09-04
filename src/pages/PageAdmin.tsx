c9587dcac8c071b4619fa6810c310579772deda6
GothicLoli <gothicyoha@gmail.com>
2021-09-04 22:44:30 +0900

    全ページにフッターを追加

--- src/pages/PageAdmin.tsx
+++ src/pages/PageAdmin.tsx
@@ -1,10 +1,14 @@
 import { Stack } from "@chakra-ui/react";
 import { ProductCard } from "../components/ProductCard";
+import { Footer } from "../components/Footer";
 
 export const PageAdmin: React.VFC = () => {
     return (
-        <Stack h="100vh" d="flex" justifyContent="center" alignItems="center">
-            <ProductCard adminMode />
-        </Stack>
+        <>
+            <Stack h="100vh" d="flex" justifyContent="center" alignItems="center">
+                <ProductCard adminMode />
+            </Stack>
+            <Footer height="15vh" backgroundColor="#e7e7e7" />
+        </>
     );
 };
--- src/pages/PageMarket.tsx
+++ src/pages/PageMarket.tsx
@@ -1,11 +1,15 @@
 import { Stack, Text } from "@chakra-ui/react";
+import { Footer } from "../components/Footer";
 import { ProductCard } from "../components/ProductCard";
 
 export const PageMarket: React.VFC = () => {
     return (
-        <Stack h="100vh" d="flex" justifyContent="center" alignItems="center">
-            <Text d="" top="0">管理者の方はURLの末尾に/adminを追加してログインしてください。</Text>
-            <ProductCard />
-        </Stack>
+        <>
+            <Stack h="100vh" d="flex" justifyContent="center" alignItems="center">
+                <Text d="" top="0">管理者の方はURLの末尾に/adminを追加してログインしてください。</Text>
+                <ProductCard />
+            </Stack>
+            <Footer height="15vh" backgroundColor="#e7e7e7" />
+        </>
     );
 };
