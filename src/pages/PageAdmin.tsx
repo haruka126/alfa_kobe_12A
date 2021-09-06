b59777d4de969f93c39137bd4c41eb83739a5705
GothicLoli <gothicyoha@gmail.com>
2021-09-06 10:07:40 +0900

    add component PageCleread

--- src/pages/PageAdmin.tsx
+++ src/pages/PageAdmin.tsx
@@ -1,13 +1,13 @@
 import { Box } from "@chakra-ui/react";
 import { ProductCard } from "../components/ProductCard";
-import { TextAnimation } from "../components/TextAnimation";
+import { PageCleared } from "./PageCleared";
 
 export const PageAdmin: React.VFC = () => {
     return (
         <>
             <Box h="100vh" d="flex" justifyContent="center" alignItems="center">
                 <ProductCard adminMode onClickDelete={() => 
-                    <TextAnimation text="あなたのゲームデータは守られました！" duration={2} />
+                    <PageCleared />
                 } />
             </Box>
             {/* PageLoginAdmin内なのでFooterが2重にならないよう、Footerを呼ばないように */}
---
+++ src/pages/PageCleared.tsx
@@ -0,0 +1,7 @@
+import { TextAnimation } from "../components/TextAnimation";
+
+export const PageCleared = () => {
+    return (
+        <TextAnimation text="あなたのゲームデータは守られました！" duration={2} />
+    );
+};
